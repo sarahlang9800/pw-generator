15fd7792a64c7dcc57e356893119e41f6667b66d
[Sarah Lang] <[lynnlang98@gmail.com]>
2022-10-31 17:13:12 -0400

    test

--- Assets/script.js
+++ Assets/script.js
@@ -1,59 +1,71 @@
 // Assignment code here
-
-// Assignment code here
+function checkNumber(passwordLength) {
+  if (!isNaN(parseFloat(passwordLength) && Number.isInteger(passwordLength))) {
+    return true
+  }
+  return false
+}
 function generatePassword() {
-  var passwordLength = prompt("How long would you like your new password to be?")
+  var isInteger
+  do {
+    var passwordLength = prompt("How long would you like your new password to be?")
+    isInteger = checkNumber(passwordLength)
+    console.log(isInteger)
+  } while (!isInteger)
 
-  // THEN I choose a length of at least 8 characters and no more than 128 characters
-  // Must return a number!!!!
+  // Returns a number of at least 8 characters and no more than 128 characters!!!!
   while (passwordLength < 8 || passwordLength > 128) {
-    console.log(typeof passwordLength)
     window.alert("The password must be an integer between 8 and 128 characters.");
     generatePassword()
   }
 
-  // ( typeof passwordLength !== 'number')
+  var input = [];
+  var password = [];
 
   var symbols = ["!", "@", "#", "$", "%", "&", "*", "(", ")", ".", "_", "+", "-", "=", "[", "]", "{", "}", "/", ":", ";", "<", ">", "?"];
   var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-  // THEN my input should be validated and at least one character type should be selected
-  var hasSymbols = window.confirm("Use symbols?"); 
-  var hasNumbers = window.confirm("Use numbers?"); 
-  var hasUpperCase = window.confirm("Use upper case?"); 
-  var hasLowerCase = window.confirm("Use lower case?");
+  // Input is validated and at least one character type MUST be selected
+  function mergeArrays(arr1, arr2) {
+    for (var i = 0; i < arr2.length; i++) {
+      arr1.push(arr2[i])
+    }
+  }
 
-  var input = [];
-  var password = [];
-
-
+  var hasSymbols = window.confirm("Use symbols?");
   if (hasSymbols) {
-    input = input.concat(symbols)
-    console.log(input)
+   mergeArrays(input, symbols)
   }
 
+  var hasNumbers = window.confirm("Use numbers?");
   if (hasNumbers) {
-    input = input.concat(numbers)
+    mergeArrays(input, numbers)
   }
 
+  var hasUpperCase = window.confirm("Use upper case?");
   if (hasUpperCase) {
-    input = input.concat(upperCase)
+    mergeArrays(input, upperCase)
   }
 
+  var hasLowerCase = window.confirm("Use lower case?");
   if (hasLowerCase) {
-    input = input.concat(lowerCase)
+    mergeArrays(input, lowerCase)
   }
-
+console.log(input)
 
   for (let i = 0; i < passwordLength; i++) {
-    let randomNumber = (Math.random() * input.passwordLength)
+    let randomNumber = Math.floor((Math.random() * input.length))
     password.push(input[randomNumber])
   }
 
-  var finalPassword = password.join("");
-
+  if (input.length > 0) {
+    return password.join("");
+  } else {
+    window.alert("Must select at least one of the options!")
+    generatePassword()
+  }
 }
 
 // Get references to the #generate element
@@ -70,22 +82,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# Password Generator Starter Code
+# sarah-lang-password-generator
+
+AS AN employee with access to sensitive data
+I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security
+
+## Acceptance Criteria 
+```
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+```
+
+## Live URL
+https://sarahlang9800.github.io/portfolio/
+
+## Link to Code
+https://github.com/sarahlang9800/portfolio
+
+## Example Photo 
+![Password Generator Example Picture](/Assets/images/pw-generator%20example%20photo.png)
+
+## Discription 
+* Asks the user for the length they would like their password to be
+* If the users input is less than 8 or greater than 128 the user is promted to select a number inbetween.
+* Gives the user four criterias and prompts them to select at least one.
+* Once the user makes all of their selections a random password is generated that fits criteria of their input.
