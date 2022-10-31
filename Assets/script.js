cab0dff634099a5a0a96ea40fc1995b0e3b4fc54
[Sarah Lang] <[lynnlang98@gmail.com]>
2022-10-31 12:45:23 -0400

    init

---
+++ Assets/script.js
@@ -0,0 +1,91 @@
+// Assignment code here
+
+// Assignment code here
+function generatePassword() {
+  var passwordLength = prompt("How long would you like your new password to be?")
+
+  // THEN I choose a length of at least 8 characters and no more than 128 characters
+  // Must return a number!!!!
+  while (passwordLength < 8 || passwordLength > 128) {
+    console.log(typeof passwordLength)
+    window.alert("The password must be an integer between 8 and 128 characters.");
+    generatePassword()
+  }
+
+  // ( typeof passwordLength !== 'number')
+
+  var symbols = ["!", "@", "#", "$", "%", "&", "*", "(", ")", ".", "_", "+", "-", "=", "[", "]", "{", "}", "/", ":", ";", "<", ">", "?"];
+  var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+  // THEN my input should be validated and at least one character type should be selected
+  var hasSymbols = window.confirm("Use symbols?"); 
+  var hasNumbers = window.confirm("Use numbers?"); 
+  var hasUpperCase = window.confirm("Use upper case?"); 
+  var hasLowerCase = window.confirm("Use lower case?");
+
+  var input = [];
+  var password = [];
+
+
+  if (hasSymbols) {
+    input = input.concat(symbols)
+    console.log(input)
+  }
+
+  if (hasNumbers) {
+    input = input.concat(numbers)
+  }
+
+  if (hasUpperCase) {
+    input = input.concat(upperCase)
+  }
+
+  if (hasLowerCase) {
+    input = input.concat(lowerCase)
+  }
+
+
+  for (let i = 0; i < passwordLength; i++) {
+    let randomNumber = (Math.random() * input.passwordLength)
+    password.push(input[randomNumber])
+  }
+
+  var finalPassword = password.join("");
+
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+
+// GIVEN I need a new, secure password
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+// WHEN asked for character types to include in the password
+// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
